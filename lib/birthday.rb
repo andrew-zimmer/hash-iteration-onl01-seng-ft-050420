04a7ac915861ef45a493422afb07923664bfbe2e
Andrew Zimmer <andrewzimmer.dev@gmail.com>
2020-05-07 18:17:49 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -15,6 +15,8 @@ def age_appropriate_birthday(hash)
   hash.each do |key, value|
     if value <= 12 
       puts "Happy Birthday #{key}! You are now #{value} years old!"
+    else 
+      puts "You are too old for this."
     end 
   end 
 end 
